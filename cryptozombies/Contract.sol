23cb143897e97d0881c85eaaceef8c14f170f351
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-19 11:35:13 -0500

    mapping

--- cryptozombies/Contract.sol
+++ cryptozombies/Contract.sol
@@ -23,6 +23,10 @@ contract ZombieFactory {
     // Create a public array of Zombie structs, and name it zombies.
     Zombie[] public zombies;
 
+    // declare mappings here
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     // Create a public function named createZombie. It should take two parameters: _name (a string), and _dna (a uint). Don't forget to pass the first argument by value by using the memory keyword.
     // Change it to a private function.
     function _createZombie(string memory _name, uint _dna) private {
